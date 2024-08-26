57598e48d54682d57a4210ac68afc6afa0ab8195
RITIKSHARMAOFFICIAL <96929769+RITIKSHARMAOFFICIAL@users.noreply.github.com>
2024-08-26 09:42:58 +0530

    Multiple

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -7,24 +7,41 @@ class DataIngestionArtifact:
 
 @dataclass
 class DataValidationArtifact:
-    pass
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
+    
 
 @dataclass
 class DataTransformationArtifact:
-    pass 
-
-@dataclass
-class ModelTrainerArtifact:
-    pass
-
-@dataclass
-class ModelEvaluationArtifact:
-    pass
-
-@dataclass
-class ModelPusherArtifact:
-    pass
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
 
 @dataclass
 class ClassificationMetricArtifact:
-    pass
+    f1_score: float
+    precision_score: float
+    recall_score: float
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
+
+@dataclass
+class ModelEvaluationArtifact:
+    is_model_accepted: bool
+    improved_accuracy: float
+    best_model_path: str
+    trained_model_path: str
+    train_model_metric_artifact: ClassificationMetricArtifact
+    best_model_metric_artifact: ClassificationMetricArtifact
+
+@dataclass
+class ModelPusherArtifact:
+    saved_model_path:str
+    model_file_path:str
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -49,40 +49,84 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_data_validation(self):
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
         try:
-            pass
+            data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
+            data_validation_artifact=data_validation.initiate_data_validation()
+            return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_data_transformation(self):
+    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
         try:
-            pass
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+            data_transformation_config=data_transformation_config)
+            
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
     
-    def start_model_trainer(self):
+    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
         try:
-            pass
+            self.model_trainer_config: ModelTrainerConfig = ModelTrainerConfig(
+                training_pipeline_config=self.training_pipeline_config
+            )
+
+            model_trainer = ModelTrainer(
+                data_transformation_artifact=data_transformation_artifact,
+                model_trainer_config=self.model_trainer_config,
+            )
+
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def start_model_evaluation(self,data_validation_artifact:DataValidationArtifact,
+                                 model_trainer_artifact:ModelTrainerArtifact,):
+        try:
+            model_evaluation_config:ModelEvaluationConfig=ModelEvaluationConfig(training_pipeline_config=self.training_pipeline_config)
+            model_eval=ModelEvaluation(model_evaluation_config,data_validation_artifact,model_trainer_artifact)
+            model_eval_artifact=model_eval.initiate_model_evaluation()
+            return  model_eval_artifact
+
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_model_evaluation(self):
+    def start_model_pusher(self,model_eval_artifact:ModelEvaluationArtifact):
         try:
-            pass
-        except Exception as e:
-            raise NetworkSecurityException(e,sys)
-        
-    def start_model_pusher(self):
-        try:
-            pass
-        except Exception as e:
-            raise NetworkSecurityException(e,sys)
+            model_pusher_config = ModelPusherConfig(training_pipeline_config=self.training_pipeline_config)
+            model_pusher = ModelPusher(model_pusher_config, model_eval_artifact)
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except  Exception as e:
+            raise  NetworkSecurityException(e,sys)
         
     def run_pipeline(self):
         try:
             data_ingestion_artifact=self.start_data_ingestion()
-            print(data_ingestion_artifact)
+            #print(data_ingestion_artifact)
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            #print(data_validation_artifact)
+            data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            #print(data_transformation_artifact)
+            
+            model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_eval_artifact=self.start_model_evaluation(data_validation_artifact=data_validation_artifact,model_trainer_artifact=model_trainer_artifact)
+            if not model_eval_artifact.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
+            print(model_eval_artifact)
+            
+            model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
+            
+            #TrainingPipeline.is_pipeline_running=False
+            #self.sync_artifact_dir_to_s3()
+            #self.sync_saved_model_dir_to_s3()
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
